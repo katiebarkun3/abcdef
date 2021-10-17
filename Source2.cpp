18d3aa5d8002b6a945afcb49a3425854bae9eb14
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2021-10-17 19:24:41 +0300

    в классе
    работа в классе 2 17102021

---
+++ Source2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+
+	/*Boolean14◦
+		.Даны три целых числа : A, B, C.
+		Проверить истинность высказывания : «Ровно одно из чисел A, B, C положительное»*/
+	/*int a, b, c;
+	bool res;
+	cin >> a >> b >> c;
+	res = (a > 0 && b < 0 && c < 0) || (a < 0 && b > 0 && c < 0) || (a < 0 && b < 0 && c > 0);
+	//res = a*b*c>0
+	cout << boolalpha << res << endl;*/
+
+
+
+	/*Boolean15◦
+		.Даны три целых числа : A, B, C.Проверить истинность высказывания :
+	«Ровно два из чисел A, B, C являются положительными».*/
+	int a, b, c;
+	bool res;
+	cin >> a >> b >> c;
+	res = a * b * c < 0;
+	cout << boolalpha << res << endl;
+	return 0;
+}

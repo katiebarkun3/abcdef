2fb819bc99d758e2a33a7aaec2501ac0a835f8de
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2021-10-31 17:57:10 +0300

    Add files via upload

---
+++ 3110212.cpp
@@ -0,0 +1,16 @@
+//Баркун Екатерина
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Russian");
+	
+	double x, y;
+	cin >> x;
+	if (x > 0) { y = 2 * x - 10; }
+	if (x == 0) { y = 0; }
+	if (x < 0) { y = 2 * abs(x) - 1; }
+	cout << y << endl;
+
+	return 0;
+}

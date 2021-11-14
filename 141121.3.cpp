d884f2ac38c33c0a8d2b197c5bbb80e8e5ae70bd
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2021-11-14 18:18:30 +0300

    Add files via upload

---
+++ 141121.3.cpp
@@ -0,0 +1,23 @@
+//Баркун Екатерина
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+	/*int a, b = 1;
+	cin >> a;
+	for (int i = 1; i <= 10; i++) {
+		b = i * a;
+		cout << a << '*' << i << '=' << b << endl;
+	}*/
+	//4
+
+	/*int a, b, k;
+	cin >> a >> b >> k;
+	for (int i = a; i <= b; i++) {
+		i += k;
+		cout << i << endl;
+		}      2*/
+
+
+	return 0;
+}

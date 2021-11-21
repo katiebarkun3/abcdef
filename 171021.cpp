d38ffc10a808bfa75d0cf1e9e030d58b60de0c3a
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2021-11-21 17:15:32 +0300

    Rename Source1.cpp to 171021.cpp

---
+++ 171021.cpp
@@ -0,0 +1,51 @@
+﻿//Баркун Екатерина П13121
+#include <iostream>
+#include <cmath>
+//Boolean1◦
+//.Дано целое число A.Проверить истинность высказывания : «Число A является положительным».
+using namespace std;
+int main () {
+//	int a;
+//	bool b;
+//	cin >> a;
+//	b = a > 0;
+//	cout << boolalpha  << b << endl;
+/*Boolean2◦
+. Дано целое число A. Проверить истинность высказывания: «Число A является нечетным».*/
+	/*int a;
+	bool b;
+	cin >> a;
+	b = a % 2 == 1;
+	cout << boolalpha << b << endl;*/
+	/*Boolean3◦
+		.Дано целое число A.Проверить истинность высказывания : «Число A является четным».
+*/
+	/*int a;
+	bool b;
+	cin >> a;
+	b = a % 2 == 0;
+	cout << boolalpha << b << endl;*/
+	/*Boolean4◦*/
+		/*.Даны два целых числа : A, B.Проверить истинность высказывания :
+	«Справедливы неравенства A > 2 и B ≤ 3».*/
+	/*int a, b;
+	bool c, d;
+	cin >> a;
+	c = a > 2;
+		cout << boolalpha << c << endl;
+		cin >> b;
+		d = b <= 3;
+		cout << boolalpha << d << endl;*/
+	/*Boolean5◦
+		.Даны два целых числа : A, B.Проверить истинность высказывания :
+	«Справедливы неравенства A ≥ 0 или B < −2»*/
+	int a, b;
+	bool c, d;
+	cin >> a;
+	c = a >= 0;
+	cout << boolalpha << c << endl;
+		cin >> b;
+	d = b < -2;
+	cout << boolalpha << d << endl; 
+	return 0;
+}

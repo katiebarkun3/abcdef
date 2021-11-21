8f505c88f39e5d6ffe38f3a095b914b382b603b8
katiebarkun3 <92594853+katiebarkun3@users.noreply.github.com>
2021-11-21 17:12:07 +0300

    Rename 08.11.21.cpp to 081121.cpp

---
+++ 081121.cpp
@@ -0,0 +1,53 @@
+﻿//Баркун Екатерина
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main() {
+
+
+
+	/*For4.Дано вещественное число — цена 1 кг конфет.Вывести стоимость 1,
+		2, . . ., 10 кг конфет.*/
+	/*double price, a;
+	cin >> price;
+	for (int i = 1; i <= 10; i++) {
+		a = price * i;
+		cout << i << ' ' << a << endl;
+	}*/
+
+
+
+	/*For7.Даны два целых числа A и B(A < B).Найти сумму всех целых чисел
+		от A до B включительно.*/
+	/*int s = 0;
+	int b, a;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		s += i;
+		cout << i << ' ' << s << endl;
+	}*/
+
+
+
+	/*For8.Даны два целых числа A и B(A < B).Найти произведение всех целых
+		чисел от A до B включительно*/
+	/*int s = 1;
+	int b, a;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		s *= i;
+		cout << i << ' ' << s << endl;
+	}*/
+
+
+
+	/*For9.Даны два целых числа A и B(A < B).Найти сумму квадратов всех целых
+		чисел от A до B включительно.*/
+	/*int b, a, s=0;
+	cin >> a >> b;
+	for (int i = a; i <= b; i++) {
+		s += i * i;
+	}
+	cout << s;*/
+	return 0;
+}
